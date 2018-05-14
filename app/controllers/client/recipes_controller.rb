ed03c8a08ed17b9fbd5613032f3ed9a3625074e7
JackMarx <seriousfools@gmail.com>
2018-05-14 18:37:32 -0500

    show

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -4,4 +4,14 @@ class Client::RecipesController < ApplicationController
     @recipes = response.body
     render 'index.html.erb'
   end
+
+  def show
+    recipe_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/recipes/#{recipe_id}")
+    @recipe = response.body
+    render 'show.html.erb'
+  end
 end
+
+
+
