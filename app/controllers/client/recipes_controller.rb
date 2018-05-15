43fc5539f73451abdc2f33caf5187bf59afbe299
JackMarx <seriousfools@gmail.com>
2018-05-14 19:49:24 -0500

    create

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -5,15 +5,32 @@ class Client::RecipesController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                    title: params[:title],
+                    chef: params[:chef],
+                    ingredients: params[:ingredients],
+                    directions: params[:directions],
+                    prep_time: params[:prep_time]
+                    }
+
+    response = Unirest.post(
+                           "http://localhost:3000/api/recipes",
+                           parameters: client_params
+                          )
+    
+    render 'create.html.erb'
+  end
+
   def show
     recipe_id = params[:id]
     response = Unirest.get("http://localhost:3000/api/recipes/#{recipe_id}")
     @recipe = response.body
     render 'show.html.erb'
-  end
-
-  def new
-    render 'new.html.erb'
   end
 end
 
